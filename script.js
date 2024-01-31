a8dcafc7d292f22821ea7a444dec9506eb75de7d
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-01-31 12:15:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+// Get the element with id "level"
+const targetElement = document.getElementById('level');
+function calculateDOMLevel(element) {
+    let level = 0;
+    let parent = element.parentElement;
+    while (parent) {
+        level++;
+        parent = parent.parentElement;
+    }
+
+    return level;
+}
+
+const domLevel = calculateDOMLevel(targetElement);
+
+alert(`The level of the element is: ${domLevel}`);
+
